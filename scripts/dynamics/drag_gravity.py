af9f8ecca2453a87add885073a74286552805e28
yunlongdong <yunlongdong@outlook.com>
2025-12-09 14:27:35 +0800

    update drag gravity

--- scripts/dynamics/drag_gravity.py
+++ scripts/dynamics/drag_gravity.py
@@ -2,7 +2,6 @@ import numpy as np
 import time
 import sys
 import csv
-import cv2
 from datetime import datetime
 import numpy as np
 
@@ -26,27 +25,12 @@ robot.run()
 beta = np.load("ls_id_beta.npy")
 dynamics_regressor = CalcDynamics()
 
-# press s to save current position into data/points.csv
+while True:
+    positions = robot.getJP()
+    velocities = robot.getJV()
 
-csv_filename = f"data/points.csv"
+    regressor = dynamics_regressor.calc(positions, velocities * 0, np.zeros(7))
 
+    tau = regressor @ beta
 
-with open(csv_filename, mode="w", newline="") as file:
-    writer = csv.writer(file)
-
-    while True:
-        positions = robot.getJP()
-        velocities = robot.getJV()
-
-        regressor = dynamics_regressor.calc(positions, velocities * 0, np.zeros(7))
-
-        tau = regressor @ beta
-
-        robot.setJTs(tau.ravel().tolist())
-
-        # check keybard
-        key = cv2.waitKey(1)
-        print(f'key: {key}')
-        if cv2.waitKey(1) != -1:
-            writer.writerow(positions)
-            print(f"save {positions}")
+    robot.setJTs(tau.ravel().tolist())
